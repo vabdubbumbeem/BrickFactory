ceb209841db0c5c3ca3a935c005edcb40740a2b9
Serezha <medv420@ya.ru>
2024-04-09 01:49:53 +0500

    Changes to be committed: 	new file:   .gitignore 	new file:   classBrick.cbp 	new file:   main.cpp
    First commit, added objects and attributes

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+enum class BrickSize:int
+{
+    Normal,
+    OneAndHalf,
+    Double
+};
+
+enum class BrickMaterial:int
+{
+    Clay,
+    CalciumSilicate,
+    FireClay
+};
+
+enum class BrickType:int
+{
+    Bilding,
+    Fire,
+    Facing
+};
+
+class Brick
+{
+protected:
+    bool Perforated = false;
+    BrickSize Size;
+    BrickMaterial Material;
+    BrickType Type;
+public:
+    bool IsPerforated() {return Perforated;}
+    void PrintPerforated() {if(Perforated){cout << "Brick is perforated\n";} else{cout << "Brick isn't perforated\n";}}
+    virtual BrickMaterial GetMaterial() = 0;
+    virtual BrickSize GetSize() = 0;
+    virtual BrickType GetType() = 0;
+
+};
+
+
+class FireBrick: public Brick
+{
+public:
+    FireBrick(BrickSize sizeBrick) {Size = sizeBrick; Type = BrickType::Fire; Material = BrickMaterial::FireClay;}
+    BrickType GetType() {return BrickType::Fire;}
+    BrickMaterial GetMaterial() {return BrickMaterial::FireClay;}
+    BrickSize GetSize() {return Size;}
+};
+
+class BildingBrick: public Brick
+{
+public:
+      BildingBrick(BrickSize sizeBrick, BrickMaterial material) {Type = BrickType::Bilding; Size = sizeBrick; Material = material;}
+      BrickType GetType() {return BrickType::Bilding;}
+      BrickMaterial GetMaterial() {return Material;}
+      BrickSize GetSize() {return Size;}
+};
+
+class FacingBrick: public Brick
+{
+public:
+      FacingBrick(BrickSize sizeBrick, BrickMaterial material) {Type = BrickType::Facing; Size = sizeBrick; Material = material;}
+      BrickType GetType() {return BrickType::Facing;}
+      BrickMaterial GetMaterial() {return Material;}
+      BrickSize GetSize() {return Size;}
+};
+
+int main()
+{
+    FireBrick Brick0001(BrickSize::Normal);
+    Brick0001.PrintPerforated();
+    return 0;
+}
